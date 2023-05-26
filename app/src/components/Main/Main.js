a25ca905ca6cb206a5c687b9d43f6efcc470d2ca
heillygalvez <31016777+heillygalvez@users.noreply.github.com>
2023-05-26 15:38:14 -0600

    style layout

--- app/src/components/Main/Main.js
+++ app/src/components/Main/Main.js
@@ -7,36 +7,42 @@ export function Main() {
   return (
     <div className={styles.main_wrapper}>
         <section className={styles.banner}>
-            <div className={styles.content}>
-                <h1>Hi, I'm Heilly</h1>
-                <h2>Data viz designer and software engineer</h2>
-                <p>I use JavaScript and Python to build interactive web tools that make data and the learnings from analyzing it accessible to both technical and non-technical audiences.</p>
-                <p>From surveys to time-series to the output of machine learning models, I'm passionate about turning information into actionable insights.</p>
-            </div>
+            <div className={styles.section_wrapper}>
+                <div className={styles.content}>
+                    <h1>Hi, I'm Heilly</h1>
+                    <h2>Data viz designer and software engineer</h2>
+                    <p>I use JavaScript and Python to build interactive web tools that make data and the learnings from analyzing it accessible to both technical and non-technical audiences.</p>
+                    <p>From surveys to time-series to the output of machine learning models, I'm passionate about turning information into actionable insights.</p>
+                </div>
             <div className={styles.image_container}>
                 <img src={headshot} alt='Heilly Galvez'/>
             </div>
+            </div>
         </section>
         <section className={styles.work}>
-            <div className={styles.content}>
-                <h1>My work</h1>
-                <h2>A few of my most recent data viz and front-end projects</h2>
-            </div>
-            <div className={styles.projects_container}>
-                {PROJECTS.map((d, i) => <div key={`project_${i}`} className={styles.project}>
-                    <img src={d.imageURL} alt={`Thumbnail for ${d.title}`} />
-                    <div className={styles.project_title}>{d.title}</div>
-                    <div className={styles.project_description}>{d.description}</div>
-                </div>)}
+            <div className={styles.section_wrapper}>
+                <div className={styles.content}>
+                    <h1>My work</h1>
+                    <h2>A few of my most recent data viz and front-end projects</h2>
+                </div>
+                <div className={styles.projects_container}>
+                    {PROJECTS.map((d, i) => <div key={`project_${i}`} className={styles.project}>
+                        <img src={d.imageURL} alt={`Thumbnail for ${d.title}`} />
+                        <div className={styles.project_title}>{d.title}</div>
+                        <div className={styles.project_description}>{d.description}</div>
+                    </div>)}
+                </div>
             </div>
         </section>
         <section className={styles.contact}>
-            <div className={styles.image_container}>
-                <iframe src="https://embed.lottiefiles.com/animation/41990"></iframe>						
-            </div>
-            <div className={styles.content}>
-            <h1>Contact me</h1>
-            <p>For work opportunities, collaborations, or anything data viz-related, please email me at heillygalvez@gmail.com.</p>
+            <div className={styles.section_wrapper}>
+                <div className={styles.image_container}>
+                    <iframe src="https://embed.lottiefiles.com/animation/41990"></iframe>						
+                </div>
+                <div className={styles.content}>
+                <h1>Contact me</h1>
+                <p>For work opportunities, collaborations, or anything data viz-related, please email me at heillygalvez@gmail.com.</p>
+                </div>
             </div>
         </section>    
     </div>
--- app/src/components/Navbar/Navbar.js
+++ app/src/components/Navbar/Navbar.js
@@ -7,15 +7,17 @@ import styles from "./Navbar.module.scss"
 
 export function Navbar() {
   return (
-    <section className={styles.navbar_wrapper}>
-        <div className={styles.logo_wrapper}>
-            <img src={logo} alt='Heilly Galvez logo' />
+    <nav className={styles.navbar_wrapper}>
+        <div className={styles.section_wrapper}>
+            <div className={styles.logo_wrapper}>
+                <img src={logo} alt='Heilly Galvez logo' />
+            </div>
+            <ul className={styles.link_wrapper}>
+                <li><a href="https://www.observablehq.com/@heillygalvez"><img src={observable_logo} alt='Observable'/></a></li>
+                <li><a href="http://www.linkedin.com/in/heillygalvez"><img src={linkedin_logo} alt='LinkedIn'/></a></li>
+                <li><a href="https://github.com/heillygalvez"><img src={github_logo} alt='Github'/></a></li>
+            </ul>
         </div>
-        <ul className={styles.link_wrapper}>
-            <li><a href="https://www.observablehq.com/@heillygalvez"><img src={observable_logo} alt='Observable'/></a></li>
-            <li><a href="http://www.linkedin.com/in/heillygalvez"><img src={linkedin_logo} alt='LinkedIn'/></a></li>
-            <li><a href="https://github.com/heillygalvez"><img src={github_logo} alt='Github'/></a></li>
-        </ul>
-    </section>
+    </nav>
   )
 }
